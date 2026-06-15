e5f24eeaa1670c584603265fa4fa5001cd358f13
syedmohammedmusharraf-INT <syedmohammed.musharraf@intglobal.com>
2026-06-15 11:35:04 +0530

    changed the deploy.sh because the docker compose and the .env moved to backend folder

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ set -e
 echo "🚀 Starting deployment..."
 
 # Check if .env file exists
-if [ ! -f .env ]; then
-    echo "❌ .env file not found! Please create one based on .env.example"
+if [ ! -f backend/.env ]; then
+    echo "❌ .env file not found! Please create one based on backend/.env.example"
     exit 1
 fi
 
@@ -15,7 +15,7 @@ echo "Git pulling latest changes..."
 git pull origin main
 
 echo "📦 Building and starting containers..."
-docker compose up -d --build
+docker compose -f backend/docker-compose.yml up -d --build
 
 echo "🧹 Cleaning up docker..."
 docker system prune -a -f
